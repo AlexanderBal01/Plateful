8293105c35f2518d2c478a866b65232e4d75a454
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-09 16:14:56 +0200

    added sealed class AppScreen to AppRouter.kt

--- app/src/main/java/com/example/plateful/navigation/AppRouter.kt
+++ app/src/main/java/com/example/plateful/navigation/AppRouter.kt
@@ -1,7 +1,18 @@
 package com.example.plateful.navigation
 
+import android.graphics.drawable.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Fastfood
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.example.plateful.R
 
 private object Routes {
     // Auth Graph Route
@@ -15,11 +26,13 @@ private object Routes {
     const val PROFILE = "profile"
     const val RANDOMFOOD = "random"
     const val FAVOURITES = "favourites"
+    const val SEARCH = "Search/{${ArgParams.SEARCH}}"
     const val FOODDETAIL = "foodDetail/{${ArgParams.FOOD_ID}}"
 }
 
 private object ArgParams {
     const val FOOD_ID = "foodId"
+    const val SEARCH = "query"
 
     fun toPath(param: String) = "{${param}}"
 }
@@ -30,3 +43,62 @@ sealed class TopLevelDestination(
     val icon: ImageVector? = null,
     val navArguments: List<NamedNavArgument> = emptyList()
 )
+
+sealed class AppScreen(val route: String) {
+    object Auth: AppScreen(Routes.AUTH) {
+        object Login: AppScreen(Routes.LOGIN)
+        object Register: AppScreen(Routes.REGISTER)
+    }
+
+    object Main: TopLevelDestination(Routes.MAIN) {
+        object Home: TopLevelDestination(
+            route = Routes.HOME,
+            title = R.string.home,
+            icon = Icons.Filled.Home,
+        )
+
+        object Profile: TopLevelDestination(
+            route = Routes.PROFILE,
+            title = R.string.profile,
+            icon = Icons.Filled.AddCircle,
+        )
+
+        object RandomFood: TopLevelDestination(
+            route = Routes.RANDOMFOOD,
+            title = R.string.random,
+            icon = Icons.Filled.Search
+        )
+
+        object Favourites: TopLevelDestination(
+            route = Routes.FAVOURITES,
+            title = R.string.favourites,
+            icon = Icons.Filled.Star
+        )
+
+        object Search: TopLevelDestination(
+            route = Routes.SEARCH,
+            title = R.string.search,
+            icon = Icons.Filled.Search,
+            navArguments = listOf(navArgument(ArgParams.SEARCH) {
+                type = NavType.Companion.StringType
+            })
+        ) {
+            fun createRoute(query: String) =
+                Routes.SEARCH
+                    .replace(ArgParams.toPath(ArgParams.SEARCH), query)
+        }
+
+        object FoodDetail: TopLevelDestination(
+            route = Routes.FOODDETAIL,
+            title = R.string.food_detail,
+            icon = Icons.Filled.Fastfood,
+            navArguments = listOf(navArgument(ArgParams.FOOD_ID) {
+                type = NavType.Companion.StringType
+            })
+        ) {
+            fun createRoute(foodId: String) =
+                Routes.FOODDETAIL
+                    .replace(ArgParams.toPath(ArgParams.FOOD_ID), foodId)
+        }
+    }
+}
