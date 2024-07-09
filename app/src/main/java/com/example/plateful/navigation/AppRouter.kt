dc13c9437d28640438b011e0b3a3e1c85a427f9b
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-09 15:18:12 +0200

    added Routes object in AppRouter

--- app/src/main/java/com/example/plateful/navigation/AppRouter.kt
+++ app/src/main/java/com/example/plateful/navigation/AppRouter.kt
@@ -1,2 +1,16 @@
 package com.example.plateful.navigation
 
+private object Routes {
+    // Auth Graph Route
+    const val AUTH = "auth"
+    const val LOGIN = "login"
+    const val REGISTER = "signup"
+
+    // Main Graph Route
+    const val MAIN = "main"
+    const val HOME = "home"
+    const val PROFILE = "profile"
+    const val RANDOMFOOD = "random"
+    const val FAVOURITES = "favourites"
+    const val FOODDETAIL = "foodDetail/{${ArgParams.FOOD_ID}}"
+}
