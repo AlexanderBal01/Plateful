ca9567f7368196f4384c69cb56edba5e440143e7
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 18:48:41 +0200

    added data class FoodListState

--- app/src/main/java/com/example/plateful/ui/screen/home/HomeUiState.kt
+++ app/src/main/java/com/example/plateful/ui/screen/home/HomeUiState.kt
@@ -1,7 +1,12 @@
 package com.example.plateful.ui.screen.home
 
+import com.example.plateful.model.Food
+
 data class HomeState(
     val scrollActionIdx: Int = 0,
     val scrollToItemIndex: Int = 0
 )
 
+data class FoodListState(
+    val foodList: List<Food> = listOf()
+)
