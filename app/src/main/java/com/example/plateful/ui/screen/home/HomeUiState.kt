9af7dfaa1cf36fecba079e5f20501ebdc31ffa08
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 19:21:07 +0200

    changed FoodListState to CategoryListState and changed sealed class to sealed interface with name CategoryApiState

--- app/src/main/java/com/example/plateful/ui/screen/home/HomeUiState.kt
+++ app/src/main/java/com/example/plateful/ui/screen/home/HomeUiState.kt
@@ -1,23 +1,24 @@
 package com.example.plateful.ui.screen.home
 
 import androidx.work.WorkInfo
-import com.example.plateful.model.Food
+import com.example.plateful.model.Category
 
 data class HomeState(
     val scrollActionIdx: Int = 0,
-    val scrollToItemIndex: Int = 0
+    val scrollToItemIndex: Int = 0,
+    val selectedCategory: Category? = null
 )
 
-data class FoodListState(
-    val foodList: List<Food> = listOf()
+data class CategoryListState(
+    val categoryList: List<Category> = listOf()
 )
 
 data class WorkerState(
     val workerInfo: WorkInfo? = null
 )
 
-sealed class FoodApiState {
-    object Success: FoodApiState
-    object Error: FoodApiState
-    object Loading: FoodApiState
+sealed interface CategoryApiState {
+    object Success: CategoryApiState
+    object Error: CategoryApiState
+    object Loading: CategoryApiState
 }
