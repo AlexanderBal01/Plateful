93fbee32e7dd9de60637c22274bc9c9b7f29ad04
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 11:47:05 +0200

    created composable PlatefulApp

---
+++ app/src/main/java/com/example/plateful/ui/PlatefulApp.kt
@@ -0,0 +1,200 @@
+package com.example.plateful.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PermanentDrawerSheet
+import androidx.compose.material3.PermanentNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.plateful.R
+import com.example.plateful.navigation.AppScreen
+import com.example.plateful.ui.components.navigation.NavComponent
+import com.example.plateful.ui.components.navigation.NavigationDrawerContent
+import com.example.plateful.ui.components.navigation.PlatefulBottomBar
+import com.example.plateful.ui.components.navigation.PlatefulChildTopAppBar
+import com.example.plateful.ui.components.navigation.PlatefulNavigationRail
+import com.example.plateful.ui.util.NavigationType
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PlatefulApp(
+    modifier: Modifier = Modifier,
+    isAuthenticated: Boolean,
+    navigationType: NavigationType,
+    rootNavHostController: NavHostController = rememberNavController()
+) {
+    val topAppbarTitle = remember { mutableStateOf("") }
+    val topAppBarState = rememberTopAppBarState()
+    val barScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(state = topAppBarState)
+
+    val showBottomBarState = rememberSaveable { (mutableStateOf(true)) }
+    val showTopBarState = rememberSaveable { (mutableStateOf(true)) }
+
+    val rootNavBackStackEntry by rootNavHostController.currentBackStackEntryAsState()
+
+    val canNavigateBack = rootNavHostController.previousBackStackEntry != null
+
+    val navigateUp: () -> Unit = { rootNavHostController.navigateUp() }
+
+    when (rootNavBackStackEntry?.destination?.route) {
+        AppScreen.Main.Home.route -> {
+            showBottomBarState.value = true
+            showTopBarState.value = true
+            topAppbarTitle.value = stringResource(AppScreen.Main.Home.title!!)
+
+        }
+
+        AppScreen.Main.Profile.route -> {
+            showBottomBarState.value = true
+            showTopBarState.value = true
+            topAppbarTitle.value = stringResource(AppScreen.Main.Profile.title!!)
+
+        }
+
+        AppScreen.Main.RandomFood.route -> {
+            showBottomBarState.value = true
+            showTopBarState.value = true
+            topAppbarTitle.value = stringResource(AppScreen.Main.RandomFood.title!!)
+        }
+
+        AppScreen.Main.Favourites.route -> {
+            showBottomBarState.value = true
+            showTopBarState.value = true
+            topAppbarTitle.value = stringResource(AppScreen.Main.Favourites.title!!)
+        }
+
+        AppScreen.Main.Search.route -> {
+            showBottomBarState.value = true
+            showTopBarState.value = true
+            topAppbarTitle.value = stringResource(AppScreen.Main.Search.title!!)
+        }
+
+        AppScreen.Main.FoodDetail.route -> {
+            showBottomBarState.value = true
+            showTopBarState.value = true
+            topAppbarTitle.value = stringResource(AppScreen.Main.FoodDetail.title!!)
+        }
+
+        else -> {
+            showBottomBarState.value = false
+            showTopBarState.value = false
+        }
+    }
+
+    when (navigationType) {
+        NavigationType.PERMANENT_NAVIGATION_DRAWER -> {
+            PermanentNavigationDrawer(
+            drawerContent = {
+                PermanentDrawerSheet (modifier.width(dimensionResource(R.dimen.drawer_width))) {
+                    NavigationDrawerContent(
+                        selectedDestination = rootNavHostController.currentDestination,
+                        onTabPressed = { node: String -> rootNavHostController.navigate(node) },
+                        modifier = modifier
+                            .wrapContentWidth()
+                            .fillMaxHeight()
+                            .background(MaterialTheme.colorScheme.primary)
+                            .padding(dimensionResource(R.dimen.drawer_padding_content))
+                    )
+                }
+            }
+            ) {
+                Scaffold(
+                    containerColor = Color.Transparent,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .nestedScroll(barScrollBehavior.nestedScrollConnection),
+                    topBar = {
+                        PlatefulChildTopAppBar(
+                            title = topAppbarTitle.value,
+                            canNavigateBack = canNavigateBack,
+                            barScrollBehavior = barScrollBehavior,
+                            navigateUp = navigateUp
+                        )
+                    }
+                ) { innerPadding ->
+                    NavComponent(
+                        navController = rootNavHostController,
+                        modifier = modifier.padding(innerPadding),
+                        isAuthenticated = isAuthenticated
+                    )
+                }
+            }
+        }
+        NavigationType.BOTTOM_NAVIGATION -> {
+            Scaffold(
+                containerColor = Color.Transparent,
+                topBar = {
+                    PlatefulChildTopAppBar(
+                        title = topAppbarTitle.value,
+                        canNavigateBack = canNavigateBack,
+                        barScrollBehavior = barScrollBehavior,
+                        navigateUp = navigateUp
+                    )
+                },
+                bottomBar = {
+                    PlatefulBottomBar(
+                        navController = rootNavHostController,
+                        selectedDestination = rootNavHostController.currentDestination
+                    )
+                }
+            ) { innerPadding ->
+                NavComponent(
+                    navController = rootNavHostController,
+                    modifier = modifier.padding(innerPadding),
+                    isAuthenticated = isAuthenticated
+                )
+            }
+        }
+        else -> {
+            Row {
+                AnimatedVisibility(visible = navigationType == NavigationType.NAVIGATION_RAIL) {
+                    PlatefulNavigationRail(
+                        modifier = modifier,
+                        selectedDestination = rootNavHostController.currentDestination,
+                        onTabPressed = { node: String -> rootNavHostController.navigate(node) }
+                    )
+                }
+                Scaffold (
+                    containerColor = Color.Transparent,
+                    topBar = {
+                        PlatefulChildTopAppBar(
+                            title = topAppbarTitle.value,
+                            canNavigateBack = canNavigateBack,
+                            barScrollBehavior = barScrollBehavior,
+                            navigateUp = navigateUp
+                        )
+                    },
+                ) { innerPadding ->
+                    // Navigation component for handling different screens.
+                    NavComponent(
+                        navController = rootNavHostController,
+                        modifier = modifier.padding(innerPadding),
+                        isAuthenticated = isAuthenticated
+                    )
+                }
+            }
+        }
+    }
+}
