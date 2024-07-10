7d3853e4e68692127b095f8d14cf3a081411b697
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 18:37:19 +0200

    removed ProfileScreen and SearchScreen

--- app/src/main/java/com/example/plateful/ui/screen/main/SearchScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.plateful.ui.screen.main
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SearchScreen(onFoodClick: () -> Unit, navigateBack: () -> Boolean) {
-    TODO("Not yet implemented")
-}
