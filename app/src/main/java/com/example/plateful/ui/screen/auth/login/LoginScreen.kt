edb9357460f5884086160a0157c9901f7e28763e
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 18:36:04 +0200

    removed Login and RegisterScreen

--- app/src/main/java/com/example/plateful/ui/screen/auth/login/LoginScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.plateful.ui.screen.auth.login
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun LoginScreen(navigateHome: () -> Unit, navigateToRegister: () -> Unit) {
-
-}
--- app/src/main/java/com/example/plateful/ui/screen/auth/register/RegisterScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.plateful.ui.screen.auth.register
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun RegisterScreen(onNavigateBack: () -> Boolean) {
-
-}
