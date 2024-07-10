d8f55d8390dff76f5bb0d2848812e2ec6c4d13e3
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 18:29:39 +0200

    removed isAuthenticated var and added right colors

--- app/src/main/java/com/example/plateful/MainActivity.kt
+++ app/src/main/java/com/example/plateful/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
@@ -16,30 +18,27 @@ import com.example.plateful.ui.theme.PlatefulTheme
 import com.example.plateful.ui.util.NavigationType
 
 class MainActivity : ComponentActivity() {
-
-    private var isAuthenticated = false
-
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PlatefulTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.onSecondary)) { innerPadding ->
                     val windowSize = calculateWindowSizeClass(activity = this)
 
                     when (windowSize.widthSizeClass) {
                         WindowWidthSizeClass.Compact -> {
-                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.BOTTOM_NAVIGATION)
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), navigationType = NavigationType.BOTTOM_NAVIGATION)
                         }
                         WindowWidthSizeClass.Medium -> {
-                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.NAVIGATION_RAIL)
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), navigationType = NavigationType.NAVIGATION_RAIL)
                         }
                         WindowWidthSizeClass.Expanded -> {
-                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.PERMANENT_NAVIGATION_DRAWER)
+                            PlatefulApp(modifier = Modifier, navigationType = NavigationType.PERMANENT_NAVIGATION_DRAWER)
                         }
                         else -> {
-                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.BOTTOM_NAVIGATION)
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), navigationType = NavigationType.BOTTOM_NAVIGATION)
                         }
                         }
                 }
