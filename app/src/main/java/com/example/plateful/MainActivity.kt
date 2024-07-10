1ae653ca285fbdc203e5c8dd5146e02512fbe50f
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 08:14:41 +0200

    added windowSize and used this for init of PlatefulApp

--- app/src/main/java/com/example/plateful/MainActivity.kt
+++ app/src/main/java/com/example/plateful/MainActivity.kt
@@ -7,41 +7,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.plateful.ui.PlatefulApp
 import com.example.plateful.ui.theme.PlatefulTheme
+import com.example.plateful.ui.util.NavigationType
 
 class MainActivity : ComponentActivity() {
+
+    private var isAuthenticated = false
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PlatefulTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val windowSize = calculateWindowSizeClass(activity = this)
+
+                    when (windowSize.widthSizeClass) {
+                        WindowWidthSizeClass.Compact -> {
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.BOTTOM_NAVIGATION)
+                        }
+                        WindowWidthSizeClass.Medium -> {
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.NAVIGATION_RAIL)
+                        }
+                        WindowWidthSizeClass.Expanded -> {
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.PERMANENT_NAVIGATION_DRAWER)
+                        }
+                        else -> {
+                            PlatefulApp(modifier = Modifier.padding(innerPadding), isAuthenticated = isAuthenticated, navigationType = NavigationType.BOTTOM_NAVIGATION)
+                        }
+                        }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlatefulTheme {
-        Greeting("Android")
     }
 }
