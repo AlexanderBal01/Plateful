711552ee3071b5ebc87ab5c19a679ba4f1527c9d
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-10 08:06:59 +0200

    added singledayregular as font and added this font to titleLarge

--- app/src/main/java/com/example/plateful/ui/theme/Type.kt
+++ app/src/main/java/com/example/plateful/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.example.plateful.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.plateful.R
+
+val SingleDayRegular = FontFamily(
+    Font(R.font.singleday_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,21 +20,11 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    titleLarge = TextStyle(
+        fontFamily = SingleDayRegular,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+    ),
 )
