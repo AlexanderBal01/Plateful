dbf31a1927ec302c05c18ff642a64d03226cc81a
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-11 02:18:01 +0200

    created composable function for CategoryList

---
+++ app/src/main/java/com/example/plateful/ui/components/home/CategoryList.kt
@@ -0,0 +1,21 @@
+package com.example.plateful.ui.components.home
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import com.example.plateful.ui.screen.home.CategoryListState
+
+@Composable
+fun CategoryList(
+    categoryListState: CategoryListState,
+    onCategoryClick: (String) -> Unit
+) {
+    val lazyListState = rememberLazyListState()
+
+    LazyColumn(state = lazyListState) {
+        items(categoryListState.categoryList) { category ->
+            CategoryItem(name = category.name, img = category.imageUrl, onCategoryClick = onCategoryClick)
+        }
+    }
+}
