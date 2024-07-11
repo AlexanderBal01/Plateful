d815a287a44cc7a482080736bfb62727a8047533
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-11 16:49:17 +0200

    created interface FoodRepository

---
+++ app/src/main/java/com/example/plateful/data/FoodRepository.kt
@@ -0,0 +1,95 @@
+package com.example.plateful.data
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.example.plateful.data.database.food.FoodDao
+import com.example.plateful.data.database.food.asDbFoodObject
+import com.example.plateful.data.database.food.asDomainFoodObject
+import com.example.plateful.data.database.food.asDomainFoodObjects
+import com.example.plateful.model.Food
+import com.example.plateful.network.FoodApiService
+import com.example.plateful.network.asDomainObject
+import com.example.plateful.network.getFoodsByCategoryAsFlow
+import com.example.plateful.workerUtils.WifiNotificationWorker
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.net.SocketTimeoutException
+import java.util.UUID
+
+interface FoodRepository {
+    fun getFood(food: String): Flow<Food?>
+
+    fun getFavourites(): Flow<List<Food>>
+
+    fun getByCategory(category: String): Flow<List<Food>>
+
+    suspend fun insertFood(food: Food)
+
+    suspend fun refresh(category: String)
+
+    var wifiWorkInfo: Flow<WorkInfo>
+}
+
+class CashingFoodRepository(
+    private val foodDao: FoodDao,
+    private val foodApiService: FoodApiService,
+    context: Context
+): FoodRepository {
+    private var workId = UUID(1,2)
+    private val workManager = WorkManager.getInstance(context)
+
+    override var wifiWorkInfo: Flow<WorkInfo> = workManager.getWorkInfoByIdFlow(workId)
+
+    override fun getFood(food: String): Flow<Food?> {
+        return foodDao.getItem(foodId = food).map {
+            it.asDomainFoodObject()
+        }
+    }
+
+    override fun getFavourites(): Flow<List<Food>> {
+        return foodDao.getFavourites().map {
+            it.asDomainFoodObjects()
+        }
+    }
+
+    override fun getByCategory(category: String): Flow<List<Food>> {
+        return foodDao.getFoodCategory(category).map {
+            it.asDomainFoodObjects()
+        }
+    }
+
+    override suspend fun insertFood(food: Food) {
+        foodDao.insert(food.asDbFoodObject())
+    }
+
+    override suspend fun refresh(category: String) {
+        val constraints = Constraints
+            .Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val requestBuilder = OneTimeWorkRequestBuilder<WifiNotificationWorker>()
+        val request = requestBuilder.setConstraints(constraints).build()
+        workManager.enqueue(request)
+        workId = request.id
+        wifiWorkInfo = workManager.getWorkInfoByIdFlow(request.id)
+
+        try {
+            foodApiService
+                .getFoodsByCategoryAsFlow(category)
+                .asDomainObject()
+                .collect {
+                    for (food in it) {
+                        insertFood(food)
+                    }
+                }
+        } catch (e: SocketTimeoutException) {
+            Log.e("REFRESH", "REFRESH: ${e.stackTraceToString()}")
+        }
+    }
+}
