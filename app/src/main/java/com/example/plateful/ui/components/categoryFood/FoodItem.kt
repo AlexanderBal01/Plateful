8e607c6a443f6a06efc05228ce8b70508428aa87
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-11 16:43:01 +0200

    created composable function FoodItem

---
+++ app/src/main/java/com/example/plateful/ui/components/categoryFood/FoodItem.kt
@@ -0,0 +1,92 @@
+package com.example.plateful.ui.components.categoryFood
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.plateful.R
+
+@Composable
+fun FoodItem(
+    modifier: Modifier = Modifier,
+    name: String,
+    img: String,
+    id: String,
+    onFoodClick: (String) -> Unit
+) {
+    Card(
+        modifier = modifier
+            .padding(dimensionResource(R.dimen.card_outer_padding))
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
+            .clickable { onFoodClick(id) }, // Use clickable for better semantics
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    ) {
+
+        Row (
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.card_padding))
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = img,
+                contentDescription = stringResource(id = R.string.img_category),
+                modifier = modifier.width(60.dp)
+            )
+            
+            Column (modifier = modifier.width(dimensionResource(id = R.dimen.text_column_width))) {
+                Text(
+                    text = name,
+                    color = MaterialTheme.colorScheme.onSecondary,
+                    fontSize = 20.sp,
+                    style = MaterialTheme.typography.bodyLarge,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+
+                )
+            }
+            // Text content
+            
+
+            // Right arrow icon
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = stringResource(R.string.navigate_to_category_item),
+                tint = MaterialTheme.colorScheme.onSecondary,
+                modifier = Modifier.size(39.dp) // Used dp instead of dimensionResource for clarity
+            )
+        }
+    }
+}
