42170e7f09974d2607c98da40bb32795c5da186d
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-11 16:48:39 +0200

    added vars favourite and category to data class Food

--- app/src/main/java/com/example/plateful/model/Food.kt
+++ app/src/main/java/com/example/plateful/model/Food.kt
@@ -3,5 +3,7 @@ package com.example.plateful.model
 data class Food(
     var id: String = "",
     var name: String = "",
-    var imageUrl: String = ""
+    var imageUrl: String = "",
+    var favourite: Boolean = false,
+    var category: String = ""
 )
