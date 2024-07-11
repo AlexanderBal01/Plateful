09b231e883e56774ceb45f9cc0aa396e0f9f9a25
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-11 16:49:49 +0200

    created interface FoodDao

---
+++ app/src/main/java/com/example/plateful/data/database/food/FoodDao.kt
@@ -0,0 +1,22 @@
+package com.example.plateful.data.database.food
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FoodDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: DbFood)
+
+    @Query("SELECT * FROM food WHERE favourite = true")
+    fun getFavourites(): Flow<List<DbFood>>
+
+    @Query("SELECT * FROM food WHERE id = :foodId")
+    fun getItem(foodId: String): Flow<DbFood>
+
+    @Query("SELECT * FROM food WHERE category = :name")
+    fun getFoodCategory(name: String): Flow<List<DbFood>>
+}
