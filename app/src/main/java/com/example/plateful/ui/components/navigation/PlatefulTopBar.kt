a033e03b7ae5dfd0158e92765b0ef3f956491af3
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-13 17:17:29 +0200

    removed scrollBehavior

--- app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulTopBar.kt
+++ app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulTopBar.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -20,11 +19,9 @@ import com.example.plateful.R
 fun PlatefulTopAppBar(
     title: String,
     canNavigateBack: Boolean,
-    barScrollBehavior: TopAppBarScrollBehavior,
     navigateUp: () -> Unit
 ) {
     CenterAlignedTopAppBar(
-        scrollBehavior = barScrollBehavior,
         title = {
             Text(title,
                 style = MaterialTheme.typography.titleLarge,
