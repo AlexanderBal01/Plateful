5842729ecd48209290c0fc666d4df43a935deb78
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-13 17:20:04 +0200

    added custom functions for navigating with the bottembar

--- app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulBottomBar.kt
+++ app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulBottomBar.kt
@@ -1,5 +1,8 @@
 package com.example.plateful.ui.components.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -7,61 +10,100 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavDestination
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
+import com.example.plateful.R
 import com.example.plateful.navigation.AppScreen
 
 @Composable
 fun PlatefulBottomBar(
-    navController: NavHostController,
+    goHome: () -> Unit,
+    goToFavourites: () -> Unit,
+    goToRandom: () -> Unit,
     selectedDestination: NavDestination?
 ) {
-    val navItems = listOf(
-        AppScreen.Main.Home,
-        AppScreen.Main.Favourites,
-        AppScreen.Main.RandomFood,
-    )
-
     NavigationBar (containerColor = MaterialTheme.colorScheme.primaryContainer) {
-        navItems.forEach { item ->
+        NavigationBarItem(
+            colors = NavigationBarItemDefaults.colors(
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary
+            ),
+            selected = selectedDestination?.route == AppScreen.Main.Home.route,
 
-            NavigationBarItem(
-                colors = NavigationBarItemDefaults.colors(
-                    MaterialTheme.colorScheme.onSecondary,
-                    MaterialTheme.colorScheme.primary,
-                    MaterialTheme.colorScheme.primary,
-                    MaterialTheme.colorScheme.onSecondary,
-                    MaterialTheme.colorScheme.onSecondary,
-                    MaterialTheme.colorScheme.onSecondary,
-                    MaterialTheme.colorScheme.onSecondary
-                ),
-                selected = selectedDestination?.route == item.route,
+            label = {
+                Text(
+                    text = stringResource(id = AppScreen.Main.Home.title!!),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = stringResource(id = AppScreen.Main.Home.title!!)
+                )
+            },
 
-                label = {
-                    Text(
-                        text = stringResource(id = item.title!!),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                },
-                icon = {
-                    Icon(
-                        imageVector = item.selectedIcon!!,
-                        contentDescription = stringResource(id = item.title!!)
-                    )
-                },
+            onClick = goHome
+        )
+        NavigationBarItem(
+            colors = NavigationBarItemDefaults.colors(
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary
+            ),
+            selected = selectedDestination?.route == AppScreen.Main.Favourites.route,
 
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-            )
-        }
+            label = {
+                Text(
+                    text = stringResource(id = AppScreen.Main.Favourites.title!!),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = stringResource(id = AppScreen.Main.Favourites.title!!)
+                )
+            },
+
+            onClick = goToFavourites
+        )
+        NavigationBarItem(
+            colors = NavigationBarItemDefaults.colors(
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary,
+                MaterialTheme.colorScheme.onSecondary
+            ),
+            selected = selectedDestination?.route == AppScreen.Main.RandomFood.route,
+
+            label = {
+                Text(
+                    text = stringResource(id = AppScreen.Main.RandomFood.title!!),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            },
+            icon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.random),
+                    contentDescription = stringResource(id = AppScreen.Main.RandomFood.title!!)
+                )
+            },
+            onClick = goToRandom
+        )
     }
 }
