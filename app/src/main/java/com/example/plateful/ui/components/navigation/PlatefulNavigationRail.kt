d7c9e7e1514619e4d4ef4e7cc7992dedfca4a820
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-13 17:18:29 +0200

    removed for and added each NavigationRailItem on its own

--- app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulNavigationRail.kt
+++ app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulNavigationRail.kt
@@ -2,6 +2,9 @@ package com.example.plateful.ui.components.navigation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationRail
@@ -9,44 +12,80 @@ import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavController
 import androidx.navigation.NavDestination
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
+import com.example.plateful.R
 import com.example.plateful.navigation.AppScreen
 
 @Composable
 fun PlatefulNavigationRail(
     modifier: Modifier = Modifier,
     selectedDestination: NavDestination?,
-    navController: NavHostController,
+    navController: NavController,
 ) {
-    val navItems = listOf(
-        AppScreen.Main.Home,
-        AppScreen.Main.Favourites,
-        AppScreen.Main.RandomFood,
-    )
 
-    NavigationRail (modifier = modifier.fillMaxHeight().background(MaterialTheme.colorScheme.onSecondary)) {
-        navItems.forEach { item ->
-            NavigationRailItem(
-                selected = selectedDestination?.route == item.route,
-                onClick = { navController.navigate(item.route) {
-                    popUpTo(navController.graph.findStartDestination().id) {
-                        saveState = true
-                    }
-                    launchSingleTop = true
-                    restoreState = true
-                } },
-                icon = {
-                    Icon(
-                        imageVector = (if (item.route == selectedDestination?.route) item.selectedIcon else item.unselectedIcon)!!,
-                        contentDescription = stringResource(id = item.title!!))
-                },
-                label = {
-                    Text(text = stringResource(id = item.title!!))
-                }
-            )
-        }
+    NavigationRail (modifier = modifier
+        .fillMaxHeight()
+        .background(MaterialTheme.colorScheme.primaryContainer)) {
+        NavigationRailItem(
+            selected = selectedDestination?.route == AppScreen.Main.Home.route,
+            onClick = {
+                navController.navigate(AppScreen.Main.Home.route)
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = stringResource(id = AppScreen.Main.Home.title!!),
+                    tint = MaterialTheme.colorScheme.onSecondary
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = AppScreen.Main.Home.title!!),
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+            }
+        )
+        NavigationRailItem(
+            selected = selectedDestination?.route == AppScreen.Main.Favourites.route,
+            onClick = {
+                navController.navigate(AppScreen.Main.Favourites.route)
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = stringResource(id = AppScreen.Main.Favourites.title!!),
+                    tint = MaterialTheme.colorScheme.onSecondary
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = AppScreen.Main.Favourites.title!!),
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+            }
+        )
+        NavigationRailItem(
+            selected = selectedDestination?.route == AppScreen.Main.RandomFood.route,
+            onClick = {
+                navController.navigate(AppScreen.Main.RandomFood.route)
+            },
+            icon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.random),
+                    contentDescription = stringResource(id = AppScreen.Main.RandomFood.title!!),
+                    tint = MaterialTheme.colorScheme.onSecondary
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = AppScreen.Main.RandomFood.title!!),
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+            }
+        )
     }
 }
