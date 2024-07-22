0ec2e561975febd654229e19b49bbfd153b56ba9
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 18:29:51 +0200

    removed DbFullMeal from database

--- app/src/main/java/com/example/plateful/data/database/PlatefulDb.kt
+++ app/src/main/java/com/example/plateful/data/database/PlatefulDb.kt
@@ -8,16 +8,12 @@ import com.example.plateful.data.database.category.CategoryDao
 import com.example.plateful.data.database.category.DbCategory
 import com.example.plateful.data.database.food.DbFood
 import com.example.plateful.data.database.food.FoodDao
-import com.example.plateful.data.database.fullMeal.DbFullMeal
-import com.example.plateful.data.database.fullMeal.FullMealDao
 
-@Database(entities = [DbCategory::class, DbFood::class, DbFullMeal::class], version = 4, exportSchema = false)
+@Database(entities = [DbCategory::class, DbFood::class], version = 5, exportSchema = false)
 abstract class PlatefulDb: RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
 
     abstract fun foodDao(): FoodDao
-
-    abstract fun fullMealDao(): FullMealDao
 
     companion object {
         @Volatile
