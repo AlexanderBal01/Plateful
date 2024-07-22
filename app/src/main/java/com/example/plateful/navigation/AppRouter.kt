fc3a8c638a3cd06ea7d015cd4da15c36fe01899f
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 18:30:59 +0200

    removed routes for randomfood and fooddetail

--- app/src/main/java/com/example/plateful/navigation/AppRouter.kt
+++ app/src/main/java/com/example/plateful/navigation/AppRouter.kt
@@ -6,15 +6,12 @@ private object Routes {
     // Main Graph Route
     const val MAIN = "main"
     const val HOME = "home"
-    const val RANDOMFOOD = "random"
     const val FAVOURITES = "favourites"
     const val CATEGORY_FOOD = "categoryFood/{${ArgParams.CATEGORY}}"
-    const val FOOD_DETAIL = "foodDetail/{${ArgParams.FOOD_ID}}"
 }
 
 private object ArgParams {
     const val CATEGORY = "category"
-    const val FOOD_ID = "foodId"
 
     fun toPath(param: String) = "{${param}}"
 }
@@ -31,24 +28,10 @@ sealed class AppScreen {
             title = R.string.home,
         )
 
-        data object RandomFood: TopLevelDestination(
-            route = Routes.RANDOMFOOD,
-            title = R.string.random,
-        )
-
         data object Favourites: TopLevelDestination(
             route = Routes.FAVOURITES,
             title = R.string.favourites,
         )
-
-        data object FoodDetail: TopLevelDestination(
-            route = Routes.FOOD_DETAIL,
-            title = R.string.food_detail
-        ) {
-            fun createRoute(foodId: String) =
-                Routes.FOOD_DETAIL
-                    .replace(ArgParams.toPath(ArgParams.FOOD_ID), foodId)
-        }
 
         data object CategoryFood: TopLevelDestination(
             route = Routes.CATEGORY_FOOD,
