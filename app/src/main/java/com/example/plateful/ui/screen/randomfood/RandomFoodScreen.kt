557ba5297ec760a3528abb245b88e837bc2dc0a1
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 14:32:52 +0200

    added string parameter in onButtonClick

--- app/src/main/java/com/example/plateful/ui/screen/randomfood/RandomFoodScreen.kt
+++ app/src/main/java/com/example/plateful/ui/screen/randomfood/RandomFoodScreen.kt
@@ -4,6 +4,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun RandomFoodScreen(onButtonClick: () -> Unit) {
+fun RandomFoodScreen(onButtonClick: (String) -> Unit) {
     Text(text = "Random")
 }
