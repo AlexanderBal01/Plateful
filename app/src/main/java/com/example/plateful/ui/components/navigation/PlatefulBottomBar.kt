f81ead73c1b9b29b213c09746f1260490ef5998d
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 18:38:26 +0200

    removed NavigationBarItem for Random Food

--- app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulBottomBar.kt
+++ app/src/main/java/com/example/plateful/ui/components/navigation/PlatefulBottomBar.kt
@@ -10,18 +10,14 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavDestination
-import com.example.plateful.R
 import com.example.plateful.navigation.AppScreen
 
 @Composable
 fun PlatefulBottomBar(
     goHome: () -> Unit,
     goToFavourites: () -> Unit,
-    goToRandom: () -> Unit,
     selectedDestination: NavDestination?
 ) {
     NavigationBar (containerColor = MaterialTheme.colorScheme.primaryContainer) {
@@ -78,32 +74,6 @@ fun PlatefulBottomBar(
             },
 
             onClick = goToFavourites
-        )
-        NavigationBarItem(
-            colors = NavigationBarItemDefaults.colors(
-                MaterialTheme.colorScheme.onSecondary,
-                MaterialTheme.colorScheme.primary,
-                MaterialTheme.colorScheme.primary,
-                MaterialTheme.colorScheme.onSecondary,
-                MaterialTheme.colorScheme.onSecondary,
-                MaterialTheme.colorScheme.onSecondary,
-                MaterialTheme.colorScheme.onSecondary
-            ),
-            selected = selectedDestination?.route == AppScreen.Main.RandomFood.route,
-
-            label = {
-                Text(
-                    text = stringResource(id = AppScreen.Main.RandomFood.title!!),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            },
-            icon = {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.random),
-                    contentDescription = stringResource(id = AppScreen.Main.RandomFood.title!!)
-                )
-            },
-            onClick = goToRandom
         )
     }
 }
