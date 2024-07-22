c84f7471b3e230df3c9a2830234c6b52c3dc5062
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 11:15:59 +0200

    created full meal data class in model layer

---
+++ app/src/main/java/com/example/plateful/model/fullMeal.kt
@@ -0,0 +1,10 @@
+package com.example.plateful.model
+
+data class FullMeal(
+    var id: String = "",
+    var name: String = "",
+    var category: String = "",
+    var area: String = "",
+    var instructions: String = "",
+    var image: String = "",
+)
