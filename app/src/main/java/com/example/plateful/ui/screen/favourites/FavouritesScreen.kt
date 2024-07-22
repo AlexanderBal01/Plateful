606f5ebdac987240a69c41807a6b19cb55541f73
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 14:35:52 +0200

    added string parameter with onFoodClick

--- app/src/main/java/com/example/plateful/ui/screen/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/example/plateful/ui/screen/favourites/FavouritesScreen.kt
@@ -4,6 +4,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun FavouritesScreen(onFoodClick: () -> Unit) {
+fun FavouritesScreen(onFoodClick: (String) -> Unit) {
     Text(text = "Favourites")
 }
