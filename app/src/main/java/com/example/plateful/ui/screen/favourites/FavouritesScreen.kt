bad2131cd2b682bb296d363f3c05a8fdd702392d
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 18:36:46 +0200

    added layout for FavouritesScreen

--- app/src/main/java/com/example/plateful/ui/screen/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/example/plateful/ui/screen/favourites/FavouritesScreen.kt
@@ -1,9 +1,40 @@
 package com.example.plateful.ui.screen.favourites
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.plateful.R
+import com.example.plateful.model.Food
+import com.example.plateful.ui.components.favourites.FavouritesList
+import com.example.plateful.ui.viewModel.PlatefulViewModel
 
 @Composable
-fun FavouritesScreen(onFoodClick: (String) -> Unit) {
-    Text(text = "Favourites")
+fun FavouritesScreen(
+    modifier: Modifier = Modifier,
+    onFoodClick: (Food) -> Unit,
+    platefulViewModel: PlatefulViewModel
+) {
+    val platefulListsState by platefulViewModel.platefulUiListsState.collectAsState()
+
+    val favouritesList = platefulListsState.favouritesList
+    Column {
+        if (favouritesList.isEmpty()) {
+            Column(modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(stringResource(id = R.string.no_favourites), color = MaterialTheme.colorScheme.primary, fontSize = 45.sp, textAlign = TextAlign.Center, lineHeight = 50.sp)
+            }
+        } else {
+            FavouritesList(modifier, favouritesList, onFoodClick)
+        }
+    }
+
 }
