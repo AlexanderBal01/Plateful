1b5bf2fb9d7a626435396b965114bc0800f1dc6f
Alexander Bal <alexander.bal@student.hogent.be>
2024-07-22 18:34:51 +0200

    removed file RandomFoodScreen

--- app/src/main/java/com/example/plateful/ui/screen/randomfood/RandomFoodScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.plateful.ui.screen.randomfood
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun RandomFoodScreen(onButtonClick: (String) -> Unit) {
-    Text(text = "Random")
-}
